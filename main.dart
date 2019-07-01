c3f2df8abcb11eac665dea6d49ec575ace4ed1a5
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2019-07-01 14:33:11 +0700

    first commit

---
+++ main.dart
@@ -0,0 +1,40 @@
+void main() {
+  String name = "Teteng";
+  int age = 23;
+  bool status = true;
+  List<String> kelas = ["papan", "penghapus"];
+  List umurSiswa = [14, 15, 16, "string", true];
+  Map siswa = {'nama': "Teteng", "age": 23};
+
+  String s1 = 'Single';
+  String s2 = "Double";
+  String s3 = 'It\'s easy';
+  String s4 = "It's easy";
+
+  String s5 =
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, quia!";
+  String s6 = "Lorem ipsum, dolor sit amet consectetur" +
+      "adipisicing elit. Facere, quia!";
+
+  String name1 = "Liong";
+  String message1 = "My name is " + name1; 
+
+  print(message1);
+
+  String name2 = "Gatot";
+  String message2 = "My name is $name2"; 
+
+  print(message2);
+
+  int a = 29;
+  int b = 10;
+
+  print("The sum of $a and $b is ${a+b}");
+
+  final cityName = "Mumbai";
+  final String countryName = "India";
+
+  const PI = 3.14;
+  const double gravity = 9.8;
+
+}
